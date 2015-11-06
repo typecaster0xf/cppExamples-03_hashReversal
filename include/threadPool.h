cb691641e6d23db9ca9b4ddd20cbee14e1881992
N. Streifling <N.Streifling@gmail.com>
2015-11-06 02:01:13 -0800

    Changed the findResult() API so that it passes an additional string parameter to 'function', and expanded its comment.

--- include/threadPool.h
+++ include/threadPool.h
@@ -16,12 +16,28 @@ public:
 	~ThreadPool();
 	
 	/*Returns the string set by "function" when
-	it eventually returns true.*/
+	it eventually returns true.
+	
+	"function" is expected to return true when it
+	has succeeded and the find operation can
+	stop.  The first parameter will be the actual
+	value found by the function, which will be
+	returned by findResult().  The second
+	parameter will be set to the value of
+	"functionArg".  The last parameter will start
+	at 0 and increment by one for each call made
+	to it by a thread, (note that this means that
+	there is no guarantee of ordering).
+	
+	"blockSize" is the number of calls to
+	"function" to assign at a time to a thread.*/
 	std::string findResult(bool (*function)(
-					std::string&, const unsigned long),
+					std::string&, const std::string,
+					const unsigned long),
+			const std::string functionArg,
 			unsigned long blockSize = 300);
 	
-	/*Used internally.  Only declaired in the
+	/*Used internally.  Only declared in the
 	public section so that it is visible to the
 	worker threads, (which for some reason can't
 	seem to access friend functions).*/
