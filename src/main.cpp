1217f9904f37915ce2ee8f17363eb15c14fbfb26
N. Streifling <N.Streifling@gmail.com>
2015-11-06 10:06:44 -0800

    Finished the main.cpp file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
+#include <cstdlib>
 #include <iostream>
+#include <string>
 #include "reverseDigest.h"
 #include "zedwood/sha512.h"
 
@@ -6,6 +8,25 @@ using namespace std;
 
 int main(const int argc, const char* argv[])
 {
-	cout << reverseDigest(sha512("rdd"), 6) << endl;
+	if(argc < 2)
+	{
+		cout << "This program takes an argument string, hashes "
+				<< "it, then reverses that hash to get the "
+				<< "original string back.  Due to the brute-force "
+				<< "algorithm used, it is best to only provided "
+				<< "short strings, otherwise this program will run "
+				<< "for quite some time.\n\n"
+				<< "Usage: " << argv[0] << " <string_to_hash> "
+				<< "<number_of_threads>" << endl;
+		return 0;
+	}
+	
+	const string stringToHash          = argv[1];
+	const unsigned int numberOfThreads = atoi(argv[2]);
+	
+	//--
+	
+	cout << "Running SHA-512 digest reversal..." << endl;
+	cout << reverseDigest(sha512(stringToHash), numberOfThreads) << endl;
 	return 0;
 }
