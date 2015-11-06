5cba94855170d6199261575ade5b85be834130e3
N. Streifling <N.Streifling@gmail.com>
2015-11-05 20:27:39 -0800

    Played with the idea of using Futures.

---
+++ include/futureLoop.h
@@ -0,0 +1,17 @@
+#pragma once
+
+template <typename T, typename R>
+class FutureLoop
+{
+public:
+	
+	FutureLoop();
+	~FutureLoop();
+	
+	bool spawnFutures(unsigned int numberOfThreads,
+			
+	
+protected:
+	
+	bool futuresAreRunning;
+};
